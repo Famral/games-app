fa248b9b04c712e47290d27e3df3d761d9b893ae
Manish Meena <108734050+ManishMeenaPage@users.noreply.github.com>
2022-08-21 16:04:16 +0530

    Add files via upload

---
+++ word-guessing/js/words.js
@@ -0,0 +1,274 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to a indoor game"
+    },
+    {
+        word: "viber",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "nepal",
+        hint: "developing country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+    {
+        word: "amazon",
+        hint: "company by the name of river"
+    }
+    {
+        word: "thames",
+        hint: "cleanest river in the world"
+    }
+    {
+        word: "exorcist",
+        hint: "most famous horror movie"
+    }
+    {
+        word: "book",
+        hint: "what has words, but never speaks"
+    }
+    {
+        word: "mp4",
+        hint: "video file format"
+    }
+    {
+        word: "pubg",
+        hint: "most popular video game"
+    }
+    {
+        word: "egg",
+        hint: "What has to be broken before you can use it"
+    }
+    {
+        word: "candle",
+        hint: "I’m tall when I’m young, and I’m short when I’m old"
+    }
+    {
+        word: "future",
+        hint: "What is always in front of you but can’t be seen"
+    }
+    {
+        word: "promise",
+        hint: "What can you break,without touching it"
+    }
+    {
+        word: "age",
+        hint: "What goes up but never comes down"
+    }  
+    {
+        word: "bank",
+        hint: "I have branches, but no fruit, trunk or leaves"
+    }  
+    {
+        word: "echo",
+        hint: "What can’t talk but will reply when spoken to?"
+    }
+    {
+        word: "shadow",
+        hint: "I follow you all the time and copy your every move, but you can’t touch me or catch me"
+    }  
+    {
+        word: "piano",
+        hint: "What has many keys but can’t open a single lock"
+    }  
+    {
+        word: "breath",
+        hint: "I’m light as a feather, yet the strongest person can’t hold me for five minutes"
+    }  
+    {
+        word: "secret",
+        hint: "If you’ve got me, you want to share me; if you share me, you haven’t kept me"
+    }  
+    {
+        word: "staircase",
+        hint: "What goes up and down but doesn’t move"
+    }  
+    {
+        word: "table",
+        hint: "What has legs, but doesn’t walk"
+    }  
+]
